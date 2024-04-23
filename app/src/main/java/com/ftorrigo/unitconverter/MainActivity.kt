418d0c1b2a961dbeef3edfd6705fe045e5f644d0
Fernando Torrigo <torrigofernando@gmail.com>
2024-04-23 19:56:12 +0100

    Adjusting layout

--- app/src/main/java/com/ftorrigo/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ftorrigo/unitconverter/MainActivity.kt
@@ -3,15 +3,21 @@ package com.ftorrigo.unitconverter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ftorrigo.unitconverter.ui.theme.UnitConverterTheme
@@ -36,17 +42,27 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter() {
     var unitValue = 0.00
 
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Unit Converter",
         )
         OutlinedTextField(value = "", onValueChange = {})
         Row {
             Box {
-                Text(text = "dasdas")
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
             }
             Box {
-                Text(text = "dasdas")
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
             }
         }
         Text(text = "Results: ")
