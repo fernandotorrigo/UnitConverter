83a51ae7710c8649249186fd27a51e0324d90728
Fernando Torrigo <torrigofernando@gmail.com>
2024-04-26 20:54:13 +0100

    Fixing screens

--- app/src/main/java/com/ftorrigo/unitconverter/MainActivity.kt
+++ app/src/main/java/com/ftorrigo/unitconverter/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.ftorrigo.unitconverter.screens.UnitConverterScreen
+import com.ftorrigo.unitconverter.ui.screens.UnitConverterScreen
 import com.ftorrigo.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/ftorrigo/unitconverter/ui/screens/UnitConverterScreen.kt
+++ app/src/main/java/com/ftorrigo/unitconverter/ui/screens/UnitConverterScreen.kt
@@ -1,4 +1,4 @@
-package com.ftorrigo.unitconverter.screens
+package com.ftorrigo.unitconverter.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
